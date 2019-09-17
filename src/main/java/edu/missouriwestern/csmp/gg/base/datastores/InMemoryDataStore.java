cf5c51c1e1bf1da7c3e33bf70b6f9fd5b7a0b0c4
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-09-17 17:02:22 -0500

    added an in-memory db for testing

---
+++ src/main/java/edu/missouriwestern/csmp/gg/base/datastores/InMemoryDataStore.java
@@ -0,0 +1,72 @@
+package edu.missouriwestern.csmp.gg.base.datastores;
+
+import edu.missouriwestern.csmp.gg.base.DataStore;
+import edu.missouriwestern.csmp.gg.base.Entity;
+import edu.missouriwestern.csmp.gg.base.HasProperties;
+import edu.missouriwestern.csmp.gg.base.Player;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class InMemoryDataStore implements DataStore {
+    private Map<Integer,Map<String,String>> entities = new ConcurrentHashMap<>();
+    private Map<String,Map<String,String>> players = new ConcurrentHashMap<>();
+
+    public void save(HasProperties obj) {
+        // implement multiple dispatch
+        if(obj instanceof Player) {
+            save((Player)obj);
+        } else if(obj instanceof Entity) {
+            save((Entity)obj);
+        }
+    }
+
+    private void save(Player player) {
+        players.put(player.getID(), player.getProperties());
+    }
+
+    private void save(Entity entity) {
+        entities.put(entity.getID(), entity.getProperties());
+    }
+
+    public void load(HasProperties obj) {
+        // implement multiple dispatch
+        if(obj instanceof Player) {
+            load((Player)obj);
+        } else if(obj instanceof Entity) {
+            load((Entity)obj);
+        }
+    }
+
+    private void load(Player player) {
+        assert players.containsKey(player.getID());
+
+        var properties = players.get(player);
+        for(var key : properties.keySet())
+            player.setProperty(key, properties.get(key));
+    }
+
+    private void load(Entity entity) {
+        assert entities.containsKey(entity.getID());
+
+        var properties = entities.get(entity);
+        for(var key : properties.keySet())
+            entity.setProperty(key, properties.get(key));
+    }
+
+    public ArrayList<Integer> search(Map<String, String> query) {
+        var setQuery = query.entrySet();
+        var results = new ArrayList<Integer>();
+        for(var id : entities.keySet()) {
+            var entityProperties = entities.get(id).entrySet();
+            if(entityProperties.containsAll(setQuery))
+                results.add(id);
+        }
+        return results;
+    }
+
+    public int getMaxEntityId() {
+        return entities.keySet().stream().mapToInt(x->x.intValue()).max().getAsInt();
+    }
+}
---
+++ src/main/java/edu/missouriwestern/csmp/gg/base/datastores/Neo4jDataStore.java
@@ -0,0 +1,176 @@
+package edu.missouriwestern.csmp.gg.base.datastores;
+
+import edu.missouriwestern.csmp.gg.base.*;
+import org.neo4j.driver.v1.*;
+import org.neo4j.driver.v1.types.Node;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+class Neo4jDataStore implements DataStore {
+
+    private Session session;
+    private Game game;
+
+    public Neo4jDataStore(String url, String user, String password) throws Exception{
+        session = GraphDatabase.driver(url, AuthTokens.basic(user, password)).session();
+    }
+
+    private static String[] GetPropertyTable(HasProperties object) {
+        String[] array = new String[2];
+        String properties = "";
+
+        for (Map.Entry properties1: object.getProperties().entrySet()) {
+            properties += String.format(" %s:\'%s\',", properties1.getKey(), properties1.getValue().toString());
+        }
+
+        if (properties.endsWith(",")) {
+            properties = properties.substring(1, properties.length() - 1);
+        }
+
+        String type = "";
+
+        if (object instanceof Player) {
+            type = "Player";
+        } else {
+            type = "Entity";
+        }
+
+        array[0] = type;
+        array[1] = properties;
+        return array;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void save(HasProperties hasProperties) {
+        StatementResult statementResult = null;
+        try {
+            statementResult = session.run(
+                    String.format("MATCH (n { id: \'%s\' })\nReturn n", hasProperties.getProperty("id"))
+            );
+        }catch(Error e){}
+        if (statementResult != null) {
+            var list = statementResult.list();
+            if (!list.isEmpty()) {
+                String[] newTable = GetPropertyTable(hasProperties);
+                session.run(String.format("MATCH (n { id: \'%s\' })\nSet n = { %s }", hasProperties.getProperty("id"), newTable[1]));
+            } else {
+                String[] newTable = GetPropertyTable(hasProperties);
+                session.run(String.format("CREATE (n:%s { %s })", newTable[0], newTable[1]));
+            }
+            if (hasProperties instanceof Container){
+                for (Entity e: ((Container) hasProperties).getEntities().collect(Collectors.toList())){
+                    save(e);
+                    session.run(
+                            String.format("MATCH (a), (b)\nWHERE a.id = \'%s\' and b.id = \'%s\'\nCREATE (a)-[holding:isHolding]->(b)")
+                    );
+                }
+            }
+        } else {
+            String[] newTable = GetPropertyTable(hasProperties);
+            session.run(String.format("CREATE (n:%s { %s })", newTable[0], newTable[1]));
+        }
+    }
+
+    @Override
+    public void load(HasProperties hasProperties) {
+        StatementResult statementResult = null;
+        try {
+            statementResult = session.run(
+                    String.format("MATCH (n { id: \'%s\' })\nReturn n", hasProperties.getProperty("id"))
+            );
+        }catch(Error e){}
+        if (statementResult != null){
+            var list = statementResult.list();
+            if (!list.isEmpty()) {
+                if (hasProperties instanceof Container){
+                    StatementResult containedObjects = null;
+                    try {
+                        containedObjects = session.run(
+                                String.format("MATCH (a)-[holding:isHolding]->(b)\nWHERE a.id = \'%s\'\nRETURN b", hasProperties.getProperty("id"))
+                        );
+                    } catch(Error e){}
+                    if (containedObjects != null){
+                        if (!containedObjects.list().isEmpty()){
+                            for (Record r: containedObjects.list()){
+                                game.getGame().getEntities().forEach(e -> {
+                                    if (e.getProperties().get("id") == r.get(0).asNode().get("id").toString()){
+                                        for (String s : r.get(0).keys()) {
+                                            e.setProperty(s, String.valueOf(r.get(s)));
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+                Node n = list.get(0).get(0).asNode();
+                for (String s : n.keys()) {
+                    hasProperties.setProperty(s, String.valueOf(n.get(s)));
+                }
+            } else {
+                save(hasProperties);
+            }
+        } else {
+            save(hasProperties);
+        }
+    }
+
+    @Override
+    public ArrayList<Integer> search(Map<String,String> map){
+        ArrayList<Integer> list = new ArrayList<>();
+        StatementResult statementResult = null;
+        String properties = "";
+
+        for (Map.Entry properties1: map.entrySet()) {
+            properties += String.format(" %s: \'%s\',", properties1.getKey(), properties1.getValue());
+        }
+
+        if (properties.endsWith(",")) {
+            properties = properties.substring(1, properties.length() - 1);
+        }
+
+        try {
+            statementResult = session.run(
+                    String.format("MATCH (n { %s })\nReturn n", properties)
+            );
+        }catch(Error e){}
+        if (statementResult != null){
+            var list1 = statementResult.list();
+            if (!list1.isEmpty()){
+                for (Record r: statementResult.list()){
+                    var id = r.get(0).asNode().get("id");
+                    list.add(id.asInt());
+                }
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public int getMaxEntityId(){
+        var returnedValue = 0;
+        StatementResult statementResult = null;
+
+        try {
+            statementResult = session.run(
+                    String.format("MATCH (n)\nReturn n")
+            );
+        }catch(Error e){}
+        if (statementResult != null) {
+            var list1 = statementResult.list();
+            if (!list1.isEmpty()) {
+                for (Record r : statementResult.list()) {
+                    var id = r.get(0).asNode().get("id");
+                    returnedValue = Math.max(returnedValue, id.asInt());
+                }
+            }
+        }
+        return returnedValue;
+    }
+}
