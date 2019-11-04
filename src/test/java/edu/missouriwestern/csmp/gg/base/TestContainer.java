3cc09fa621d7d4369b63aa6edc42b8fe1fe5efe7
McKFlat <38057035+mckflat@users.noreply.github.com>
2019-11-03 20:47:02 -0600

    added tests for Container and created Map2 for test doubles

--- src/test/java/edu/missouriwestern/csmp/gg/base/TestContainer.java
+++ src/test/java/edu/missouriwestern/csmp/gg/base/TestContainer.java
@@ -1,4 +1,45 @@
 package edu.missouriwestern.csmp.gg.base;
 
+import edu.missouriwestern.csmp.gg.base.datastores.InMemoryDataStore;
+import org.junit.Test;
+import static junit.framework.TestCase.*;
+
 public class TestContainer {
+    DummyGame game = new DummyGame();
+    DummyEntity ent = new DummyEntity(game);
+
+
+    @Test
+    public void testGetGame(){
+
+    }
+
+    @Test
+    public void testAddEntity(){
+        game.addEntity(ent);
+    }
+
+    @Test
+    public void removeEntity(){
+
+        assertEquals(0, game.getEntities().count());
+    }
+
+    @Test
+    public void getEntities(){
+
+        assertEquals(0,game.getEntities().count());
+
+    }
+
+    @Test
+    public void isEmpty(){
+        //if we count game as an entity, this test is correct
+        assertEquals(false,game.isEmpty());
+    }
+
+    @Test
+    public void containsEntity(){
+
+    }
 }
