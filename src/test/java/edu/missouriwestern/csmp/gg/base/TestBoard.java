0af8e653c0b077665ee226402c62a99e1914141e
McKFlat <mmckinney3@missouriwestern.edu>
2019-11-12 23:50:36 -0600

    Added tests to TestBoard

--- src/test/java/edu/missouriwestern/csmp/gg/base/TestBoard.java
+++ src/test/java/edu/missouriwestern/csmp/gg/base/TestBoard.java
@@ -1,4 +1,75 @@
 package edu.missouriwestern.csmp.gg.base;
 
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
 public class TestBoard {
+    DummyTile tile = new DummyTile(0,0);
+    DummyBoard board = new DummyBoard("unsmart-board", new Map1().toString(), tile);
+    DummyGame game = new DummyGame(board);
+
+    @Test
+    public void testGetGame(){
+        assertEquals(game, board);
+    }
+
+    @Test
+    public void testSetGame(){
+        DummyGame tempGame = new DummyGame();
+        board.setGame(tempGame);
+        assertEquals(tempGame, board.getGame());
+    }
+
+    @Test
+    public void testGetAdjacentTile(){
+
+    }
+
+    @Test
+    public void testGetAdjacentTileDirection(){
+
+    }
+
+    @Test
+    public void testGetTiles(){
+
+    }
+
+    @Test
+    public void testGetTileStream(){
+
+    }
+
+    @Test
+    public void TestGetTile(){
+
+    }
+
+    @Test
+    public void TestGetName(){
+
+    }
+
+    @Test
+    public void testGetWidth(){
+
+    }
+
+    @Test
+    public void testGetHeight(){
+
+    }
+    @Test
+    public void testGetTileMap(){
+
+    }
+
+    @Test
+    public void testLoadMap(){
+
+    }
+
 }
