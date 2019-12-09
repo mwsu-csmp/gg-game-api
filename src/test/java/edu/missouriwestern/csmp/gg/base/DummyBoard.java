6a8ad0bb2ae320b65badf51edcc29a661c51843e
McKFlat <mmckinney3@missouriwestern.edu>
2019-12-09 04:01:25 -0600

    Dummyboard constructor now takes arbitrary number of tiles

--- src/test/java/edu/missouriwestern/csmp/gg/base/DummyBoard.java
+++ src/test/java/edu/missouriwestern/csmp/gg/base/DummyBoard.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class DummyBoard extends Board {
 
-    public DummyBoard(String name, String charMap, Tile tile){
+    public DummyBoard(String name, String charMap, Tile ... tile){
         super(name,charMap, Map.of(), tile);
 
     }
